daf56f959602ee88f1e760e58260ba05dad22635
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-09 20:49:53 +0530

    Added CustomerService

---
+++ Back_End/src/main/java/lk/ijse/spring/service/CustomerService.java
@@ -0,0 +1,23 @@
+package lk.ijse.spring.service;
+
+import lk.ijse.spring.dto.CustomerDTO;
+
+import java.util.List;
+
+/**
+ * @author : Gathsara
+ * created : 10/9/2023 -- 8:48 PM
+ **/
+
+public interface CustomerService {
+
+    void addCustomer(CustomerDTO dto);
+
+    void deleteCustomer(String id);
+
+    List<CustomerDTO> getAllCustomer();
+
+    CustomerDTO findCustomer(String id);
+
+    void updateCustomer(CustomerDTO c);
+}
