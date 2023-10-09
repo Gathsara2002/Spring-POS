ede71d5ab1933011db23576a74f09a02889e4774
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-09 20:24:49 +0530

    Added OrderDetailsDTO

--- Back_End/src/main/java/lk/ijse/spring/dto/OrderDetailsDTO.java
+++ Back_End/src/main/java/lk/ijse/spring/dto/OrderDetailsDTO.java
@@ -1,8 +1,24 @@
 package lk.ijse.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
 /**
  * @author : Gathsara
  * created : 10/9/2023 -- 8:23 PM
  **/
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class OrderDetailsDTO {
+    private String oid;
+    private String itemCode;
+    private int qty;
+    private BigDecimal unitPrice;
 }
