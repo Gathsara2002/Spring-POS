1c137a25a01811fb372af347505038312c73cdfb
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-09 21:07:55 +0530

    Added ItemServiceImpl

---
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/ItemServiceImpl.java
@@ -0,0 +1,37 @@
+package lk.ijse.spring.service.impl;
+
+import lk.ijse.spring.dto.ItemDTO;
+import lk.ijse.spring.service.ItemService;
+
+import java.util.List;
+
+/**
+ * @author : Gathsara
+ * created : 10/9/2023 -- 9:07 PM
+ **/
+public class ItemServiceImpl implements ItemService {
+    @Override
+    public void addItem(ItemDTO dto) {
+
+    }
+
+    @Override
+    public void deleteItem(String id) {
+
+    }
+
+    @Override
+    public List<ItemDTO> getAllItem() {
+        return null;
+    }
+
+    @Override
+    public ItemDTO findItem(String id) {
+        return null;
+    }
+
+    @Override
+    public void updateItem(ItemDTO c) {
+
+    }
+}
