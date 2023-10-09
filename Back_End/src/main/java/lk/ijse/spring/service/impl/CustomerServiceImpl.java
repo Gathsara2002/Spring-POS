9c025692a87ddb8bfbfaea973b02b716382e2b34
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-09 21:07:06 +0530

    Added CustomerServiceImpl

---
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,37 @@
+package lk.ijse.spring.service.impl;
+
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.service.CustomerService;
+
+import java.util.List;
+
+/**
+ * @author : Gathsara
+ * created : 10/9/2023 -- 9:06 PM
+ **/
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public void addCustomer(CustomerDTO dto) {
+
+    }
+
+    @Override
+    public void deleteCustomer(String id) {
+
+    }
+
+    @Override
+    public List<CustomerDTO> getAllCustomer() {
+        return null;
+    }
+
+    @Override
+    public CustomerDTO findCustomer(String id) {
+        return null;
+    }
+
+    @Override
+    public void updateCustomer(CustomerDTO dto) {
+
+    }
+}
