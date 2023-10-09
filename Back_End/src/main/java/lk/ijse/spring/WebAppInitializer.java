75e491ebe4ddcde79d037c78e36f0621ddaa25b8
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-09 20:26:39 +0530

    WebAppInitializer updated

--- Back_End/src/main/java/lk/ijse/spring/WebAppInitializer.java
+++ Back_End/src/main/java/lk/ijse/spring/WebAppInitializer.java
@@ -1,8 +1,27 @@
 package lk.ijse.spring;
 
+import lk.ijse.spring.config.WebAppConfig;
+import lk.ijse.spring.config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 /**
  * @author : Gathsara
  * created : 10/9/2023 -- 8:10 PM
  **/
-public class WebAppInitializer {
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
