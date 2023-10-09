1c271b51b3ed5c35b13600e1b0bca9aad712118d
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-09 20:16:14 +0530

    Added WebRootConfig class

---
+++ Back_End/src/main/java/lk/ijse/spring/config/WebRootConfig.java
@@ -0,0 +1,26 @@
+package lk.ijse.spring.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * @author : Gathsara
+ * created : 10/9/2023 -- 8:15 PM
+ **/
+
+@Configuration
+//@Import({JPAConfig.class})
+@ComponentScan(basePackages = "lk.ijse.spring.service")
+public class WebRootConfig {
+    public WebRootConfig() {
+        System.out.println("WebRootConfig : Instantiated");
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
